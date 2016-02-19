78fbfe8656ec7e0a0d7ce91e29db97dc212adb70
Dan Allen <dan.j.allen@gmail.com>
2016-02-19 05:39:00 -0700

    make it so

---
+++ dist/bespoke-extern.js
@@ -0,0 +1,28 @@
+/*!
+ * bespoke-extern v1.0.0-dev
+ *
+ * Copyright 2016, Dan Allen
+ * This content is released under the MIT license
+ */
+
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g=(g.bespoke||(g.bespoke = {}));g=(g.plugins||(g.plugins = {}));g.extern = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports = function(core) {
+  return function(deck) {
+    var g = window;
+    if (core) {
+      if (g.bespoke instanceof Object && Array.isArray(g.bespoke.decks)) core.decks = g.bespoke.decks;
+      g.bespoke = core;
+    }
+    else if (g.bespoke instanceof Object) core = g.bespoke;
+    else core = g.bespoke = {};
+    (Array.isArray(core.decks) ? core.decks : (core.decks = [])).push(core.deck = deck);
+    deck.on('destroy', function() {
+      var idx = core.decks.indexOf(deck);
+      if (idx >= 0) core.decks.splice(idx, 1);
+      core.deck = undefined;
+    });
+  };
+};
+
+},{}]},{},[1])(1)
+});

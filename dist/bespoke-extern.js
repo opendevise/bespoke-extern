e4c69d60b3a1675158d0e54077c91b79e701d391
Dan Allen <dan.j.allen@gmail.com>
2016-02-20 18:32:05 -0700

    add support for options to control object, name and scope

--- dist/bespoke-extern.js
+++ dist/bespoke-extern.js
@@ -6,20 +6,18 @@
  */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g=(g.bespoke||(g.bespoke = {}));g=(g.plugins||(g.plugins = {}));g.extern = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = function(core) {
+module.exports = function(opts) {
   return function(deck) {
-    var g = window;
-    if (core) {
-      if (g.bespoke instanceof Object && Array.isArray(g.bespoke.decks)) core.decks = g.bespoke.decks;
-      g.bespoke = core;
-    }
-    else if (g.bespoke instanceof Object) core = g.bespoke;
-    else core = g.bespoke = {};
-    (Array.isArray(core.decks) ? core.decks : (core.decks = [])).push(core.deck = deck);
+    opts = opts && (opts.object || opts.name || opts.scope) ? opts : { object: opts };
+    var bespoke = opts.object, name = opts.name || 'bespoke', scope = opts.scope || window;
+    if (bespoke) scope[name] = bespoke;
+    else if (scope[name]) bespoke = scope[name];
+    else bespoke = (scope[name] = {});
+    (Array.isArray(bespoke.decks) ? bespoke.decks : (bespoke.decks = [])).push(bespoke.deck = deck);
     deck.on('destroy', function() {
-      var idx = core.decks.indexOf(deck);
-      if (idx >= 0) core.decks.splice(idx, 1);
-      core.deck = undefined;
+      var idx = bespoke.decks.indexOf(deck);
+      if (idx >= 0) bespoke.decks.splice(idx, 1);
+      delete bespoke.deck;
     });
   };
 };
